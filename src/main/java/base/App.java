a6f4ad12961ce88cff6857f4ef83f4540f483005
paulshannon-coding <mikey4295@hotmail.com>
2021-06-04 16:18:01 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,50 @@
+package base;
+
+import java.util.Scanner;
+
+public class App
+{
+    Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args)//main
+    {
+        //creating app instance
+        App madLib = new App();
+
+        //prompt for input
+        System.out.print("Enter a noun: ");
+        String nounOne = madLib.new_Noun();
+
+        System.out.print("Enter a verb: ");
+        String verbOne = madLib.new_Verb();
+
+        System.out.print("Enter an adjective: ");
+        String adjectiveOne = madLib.new_adjective();
+
+        System.out.print("Enter an adverb: ");
+        String adverbOne = madLib.new_adverb();
+
+        System.out.printf("How is it that my %s always %s when my %s neighbor sits on the toilet %s?", nounOne, verbOne, adjectiveOne, adverbOne);
+    }
+    public String new_Noun()
+    {
+        String noun = input.nextLine();
+        return noun;
+    }
+    public String new_Verb()
+    {
+        String verb = input.nextLine();
+        return verb;
+    }
+    public String new_adjective()
+    {
+        String adjective = input.nextLine();
+        return adjective;
+    }
+    public String new_adverb()
+    {
+        String adverb = input.nextLine();
+        return adverb;
+    }
+}
+
